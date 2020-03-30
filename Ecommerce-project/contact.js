4159caf8a8cc4804f812848998bb16c3968e456b
Josh McNeill <joshmcneill@macbook-pro.attlocal.net>
2020-03-30 19:53:53 -0400

    turn-in

---
+++ Ecommerce-project/contact.js
@@ -0,0 +1,21 @@
+
+function validateForm() {
+    var x = document.forms["myForm"]["name"].value;
+    var y = document.forms["myForm"]["email"].value;
+    if (x == "" && y == "") {
+        alert("Please fill out Name and Email.");
+        return false;
+    } else if (x == "") {
+        alert("Please fill out your Name.");
+        return false;
+    } else if (y == "") {
+        alert("Please fill out your Email.");
+        return false;
+    } else {
+        return true
+    }  
+}   
+ window.onload.alert = noalert.validateForm(true);
+
+  validateForm();
+

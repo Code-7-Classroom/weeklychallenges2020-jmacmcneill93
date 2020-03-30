6ee2b91feb5981201c20f5865298815a32467a45
Josh McNeill <61425168+jmacmcneill93@users.noreply.github.com>
2020-03-30 11:12:04 -0400

    Add files via upload
    checkpoin-array

---
+++ array-checkpoint/index.js
@@ -0,0 +1,12 @@
+var drinks = new Array();
+drinks.push("water");
+drinks.push("coffee");
+drinks.push("tea");
+console.log('drinks');
+
+var foods = newArray();
+foods.push("apple");
+foods.push("sandwich");
+foods.push("chips");
+
+var meals = (foods + drinks);
